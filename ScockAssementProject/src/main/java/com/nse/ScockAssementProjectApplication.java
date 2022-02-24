7033ce7f4658938ebfd5da37d4e6210d30b84151
Mohitkalambe98 <100348692+Mohitkalambe98@users.noreply.github.com>
2022-02-24 15:44:04 +0530

    Add files via upload

---
+++ ScockAssementProject/src/main/java/com/nse/ScockAssementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScockAssementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScockAssementProjectApplication.class, args);
+	}
+
+}
